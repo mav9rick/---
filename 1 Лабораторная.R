c2c7929a5177cd03358fa4cb211c093e3d5c3758
mav9rick <koil-3@yandex.ru>
2024-10-27 14:27:18 +0500

    Реализована функция отображения графика printFunc

--- 1 Лабораторная.R
+++ 1 Лабораторная.R
@@ -1,12 +1,26 @@
 #1 lab ЭММ
-theta <- 
-AR <- function (tetta)
+n = 500
+t1 = 0.3
+t2 = 1
+t3 = 3
+tetta <- c(t1,t2,t3) 
+AR <- function (tetta,n)
 {
-  x <- numeric
+  x <- numeric(n)
   x[1] <- rnorm(1)
   for (k in 2:n) 
   {
-    x[k] <- theta * x[k-1] + rnorm(1)
+    x[k] <- tetta * x[k-1] + rnorm(1)
   }
   return(x)
 }
+AR1 = AR(t1,n)
+AR2 = AR(t2,n)
+AR3 = AR(t3,n)
+printFunc <- function (data)
+{
+  plot(data, type="l", col = "green", main = "Линейный график", xlab = "Наблюдение", ylab = "Значения") 
+}
+printFunc(AR1)
+printFunc(AR2)
+printFunc(AR3)
